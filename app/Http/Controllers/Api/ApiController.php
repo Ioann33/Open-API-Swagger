41f7c9f7576fdf02f6cd91717d20c8504d7486c8
ioann <ivan.karmalyk2017@gmail.com>
2023-02-19 22:48:16 +0200

    added photo optimization functionality

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Tinify\Source;
+use Tinify\Tinify;
 
 class ApiController extends Controller
 {
@@ -29,11 +31,21 @@ class ApiController extends Controller
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
 
-        $path = '/storage/'.$request->file('photo')->store('photos', 'public');
+        $path = 'storage/'.$request->file('photo')->store('origin', 'public');
+        Tinify::setKey('9vknXbc3fVnGFy1sTYJ6qr3R7gV7rdYV');
+        $path_info = pathinfo($path);
+        $res_pass = "storage/photos/{$path_info['filename']}.jpg";
+        $source = Source::fromFile($path)->convert(["type"=>"image/jpg"]);
+        $source->resize([
+            "method" => "fit",
+            "width" => 70,
+            "height" => 70
+        ])->toFile($res_pass);
+        unlink($path);
         User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'photo' => $path,
+            'photo' => $res_pass,
             'password' => Hash::make($request->password),
         ]);
         return response()->json([], 204);
