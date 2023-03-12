97e5c663963ce28297d9550ef2371edb597bfec8
ioann <ivan.karmalyk2017@gmail.com>
2023-03-12 17:40:08 +0200

    added api doc and describe get request

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -9,8 +9,62 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+/**
+ * @OA\Info(
+ *     title="Api Documentation",
+ *     version="1.0.0",
+ *     @OA\Contact(
+ *          email="ivan@gmail.com"
+ *      ),
+ *     @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ * @OA\Tag(
+ *     name="User",
+ *     description="You can add and see users here"
+ * )
+ * @OA\Server(
+ *     description="Api Documentation",
+ *     url="http://127.0.0.1:8000/api"
+ * ),
+ * @OA\SecurityScheme(
+ *     type="apiKey",
+ *     in="header",
+ *     name="Authorization",
+ *     securityScheme="Bearer defrfgiuhdwiquhgdiuwe324fem3kmerf"
+ * )
+ */
 class ApiController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @OA\Get(
+     *  path="/users",
+     *  operationId="usersAll",
+     *  tags={"User"},
+     *  summary="Get All users with count per page",
+     *  @OA\Parameter(
+     *     name="perPage",
+     *     in="query",
+     *     description="The number item per page",
+     *     required=false,
+     *     @OA\Schema(
+     *          type="integer"
+     *     )
+     * ),
+     *  @OA\Response(
+     *     response="200",
+     *     description="status ok",
+     * ),
+     *  @OA\Response(
+     *     response="500",
+     *     description="server error",
+     * ),
+     *)
+     */
     public function getUsers(Request $request): JsonResponse
     {
         $perPage = $request->perPage ?? 6;
